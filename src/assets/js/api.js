47807ccb002b4d6f29b7f4ef126f2e2a84c51146
Manu <manuelass.dev@gmail.com>
2025-09-24 21:37:08 -0300

    feat: buscar pokemons na api

---
+++ src/assets/js/api.js
@@ -0,0 +1,17 @@
+export async function fetchPokemons(offset, limit) {
+  const res = await fetch(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`);
+  const data = await res.json();
+
+  const promises = data.results.map(async (pokemon) => {
+    const pokeRes = await fetch(pokemon.url);
+    const pokeData = await pokeRes.json();
+    return {
+      id: pokeData.id,
+      name: pokeData.name,
+      types: pokeData.types.map(t => t.type.name),
+      image: pokeData.sprites.other['official-artwork'].front_default
+    };
+  });
+
+  return { pokemons: await Promise.all(promises), total: data.count };
+}
