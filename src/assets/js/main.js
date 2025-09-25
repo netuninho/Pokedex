1a07fa50c57bd27400c236f561c5d538a9981065
Manu <manuelass.dev@gmail.com>
2025-09-24 21:48:54 -0300

    feat: criando paginacao

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,18 +1,48 @@
 import '../scss/main.scss';
 import { createPokemonCard } from './dom.js';
 import { fetchPokemons } from './api.js';
+import { renderPagination } from './pagination.js';
 
 const container = document.getElementById('pokemon-list');
-let offset = 0;
-const limit = 18;
+const paginationNumbers = document.getElementById('page-buttons');
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
 
-async function renderPokemons() {
+let currentPage = 1;
+const limit = 18;
+let totalPokemons = 0;
+
+async function renderPokemons(page = 1) {
+  const offset = (page - 1) * limit;
   try {
-    const { pokemons } = await fetchPokemons(offset, limit);
+    const { pokemons, total } = await fetchPokemons(offset, limit);
+    totalPokemons = total;
+
+    container.innerHTML = '';
     pokemons.forEach(pokemon => createPokemonCard(pokemon, container));
+
+    renderPagination(currentPage, totalPokemons, limit, paginationNumbers, prevBtn, nextBtn, (page) => {
+      currentPage = page;
+      renderPokemons(page);
+    });
   } catch (error) {
     console.error('Erro ao buscar pokémons:', error);
   }
 }
 
-renderPokemons();
+prevBtn.addEventListener('click', () => {
+  if (currentPage > 1) {
+    currentPage--;
+    renderPokemons(currentPage);
+  }
+});
+
+nextBtn.addEventListener('click', () => {
+  const totalPages = Math.ceil(totalPokemons / limit);
+  if (currentPage < totalPages) {
+    currentPage++;
+    renderPokemons(currentPage);
+  }
+});
+
+renderPokemons(currentPage);
---
+++ src/assets/js/pagination.js
@@ -0,0 +1,18 @@
+export function renderPagination(currentPage, totalItems, limit, container, prevBtn, nextBtn, onPageChange) {
+  const totalPages = Math.ceil(totalItems / limit);
+  container.innerHTML = '';
+
+  for (let i = 1; i <= totalPages; i++) {
+    if (i > currentPage - 3 && i < currentPage + 3) {
+      const pageBtn = document.createElement('button');
+      pageBtn.textContent = i;
+      pageBtn.classList.add('pagination__number');
+      if (i === currentPage) pageBtn.classList.add('active');
+      pageBtn.addEventListener('click', () => onPageChange(i));
+      container.appendChild(pageBtn);
+    }
+  }
+
+  prevBtn.disabled = currentPage === 1;
+  nextBtn.disabled = currentPage === totalPages;
+}
